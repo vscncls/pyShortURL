d04faa62c8f37c3a9821b379913923028d7b4455
Lucas Lourenco Vasconcelos <lucaslou4@gmail.com>
2019-07-26 23:48:12 -0300

    implemented features

--- api.py
+++ api.py
@@ -17,14 +17,16 @@ class Urls(db.model):
     url = db.Column(db.String(1000), unique=True)
     # unique because if there were >1 there'd be conflicts
     shortenend_url = db.Column(db.String(50), unique=True)
+    # 1 = random, 2 = custom
+    type = db.Column(db.Integer)
 
 class Url:
     def __init__(self, url):
         self.url = str(url)
-        self.shortened = self.shorten()
 
     def shorten(self, size='8'):
-	"""Shotens an URL, sets self.shortened and returns shotened URL"""
+	"""Shotens an URL, sets self.shortenend and returns shotened URL, sets type to random(1)"""
+        # upper/lower case and nums
         chars = ascii_uppercase + ascii_lowercase + '0123456789'
 
         # defaults to true so it enters the loop
@@ -35,57 +37,111 @@ class Url:
             if not self.get_from_short(short).url:
                 duplicate = False
 
-        self.shortened = short
+        self.shortenend = short
+        self.type = 1
         return short
+
+    def custom_url(self, url):
+        """Sets url to given url and changes type to Custom(2)"""
+        self.shortenend_url = url
+        self.type = 2
+        return True
 
     def persist(self):
         """Saves object to the db."""
-        return True
+        try:
+            Urls(url=self.url, shortenend_url=self.shortenend_url, type=2)
+            db.session.add(_url)
+            db.session.commit()
+            logger.info("Saved url: {}, short: {}, id: {}".format(self.url,self.shortenend_url,self.id_url))
+            return True
+        except SQLAlchemyError as error:
+            logger.error(error.args)
+            return False
 
-    def get_from_id(self, id_url):
+    @staticmethod
+    def get_from_id(id_url):
         """Returns an url object based on given id."""
-        obj = Urls.query.filter_by(id_url=id_url).first()
+        q = Urls.query.filter_by(id_url=id_url).first()
+        obj = Url(q.url)
+        obj.id = q.url_id
+        obj.url = q.url
+        obj.shortenend_url = q.shortenend_url
+        obj.type = q.type
+        return self
 
-        return obj
-
-    def get_from_url(self, url):
+    @staticmethod
+    def get_from_url(url, type=1):
         """Returns an url object based on given url."""
-        obj = Urls.query.filter_by(url=url)
-
+        q = Urls.query.filter_by(url=url, type=type).first()
+        obj = Url(q.url)
+        obj.id = q.url_id
+        obj.url = q.url
+        obj.shortenend_url = q.shortenend_url
+        obj.type = q.type
         return obj
 
-    def get_from_short(self, short_url):?
-        """Returns an url object based on giver shortened url."""
-        obj = Url.query.filter_by(shortened_url=short_url)
-
+    @staticmethod
+    def get_from_short(short_url):
+        """Returns an url object based on giver shortenend url."""
+        q = Urls.query.filter_by(shortenend_url=short_url).first()
+        obj = Url(q.url)
+        obj.id = q.url_id
+        obj.url = q.url
+        obj.shortenend_url = q.shortenend_url
+        obj.type = q.type
         return obj
-
-@app.route('/', methods=['GET'])
-def index():
-    """Landing page that asks for an url."""
-    # TODO: add landing page
-    return ''
 
 @app.route('/shorten', methods=['POST'])
 def shorten_url():
-    """Shortens url"""
+    """Shortens url."""
     data = request.get_json()
-    # TODO: verify if url is already inserted
+    q_url = Url.get_from_url(data['url']).url
+    if q_url:
+        return jsonfy({
+                'msg': 'ok',
+                'method': 'already exists',
+                'url': q_url
+            })
     _url = Url(data['url'])
     _url.persist()
 
     return jsonfy({
-        'status': 'Ok',
+        'msg': 'Ok',
         'method': 'new'
-        'url': n_url.shortened
+        'url': _url.shortenend
         })
 
 @app.route('/u/<url>', methods=['GET'])
 def redirect(url):
-    """Uses the shortened url to redirect to the actual URL."""
-    _url = Url.get_from_url(url)
+    """Returns the actual URL."""
+    try:
+        _url = Url.get_from_url(url=url, type=1).url
+    except AttributeError:
+        return jsonfy({
+                'msg': 'not found',
+                'url': None
+            })
 
-    return redirect(_url.url)
+    return jsonfy({
+            'msg': 'ok',
+            'url': _url
+        })
+@app.route('/c/<url>', methods=['GET'])
+def redirect_custom(url):
+    """Returns custom url."""
+    try:
+        _url = Url.get_from_url(url=url, type=2).url
+    except AttributeError:
+        return jsonfy({
+                'msg':'not found',
+                'url': None
+            })
+
+    return jsonfy({
+                'msg': 'ok',
+                'url': _url
+            )}
 
 if __name__ == '__main__':
     # binds to port if defined else defaults to 8000
